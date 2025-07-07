da4fc665801a1378945130b865bd0b5ca524c571
Laura R Lab automation computer <laura.rz2992@gmail.com>
2025-07-07 11:59:24 +0300

    PULLUP moved to iterrupt pin

--- Project5-Reacting using interrupts/button_light_coe_with_interrupt.ino
+++ Project5-Reacting using interrupts/button_light_coe_with_interrupt.ino
@@ -2,18 +2,32 @@ const int buttonPin = 6;   // Grove button pin
 const int ledPin = 4;      // Grove LED pin
 const int interruptPin = 3; // Interrupt pin (jumpered from pin 6); when pin 3 is activated, it is 1 (high)
 
+int buttonState = 0
+
 volatile bool interruptTriggered = false;
 
-void handleInterrupt() {
-  interruptTriggered = true;
-}
-
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);         // Grove button is on D6
-  pinMode(interruptPin, INPUT);      // Set D2 (jumpered from D6) as input
+  pinMode(buttonPin, INPUT);         // Grove button is on D6
+  pinMode(interruptPin, INPUT_PULLUP);      // Set D2 (jumpered from D6) as input
   pinMode(ledPin, OUTPUT);           // LED on D4
   attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, CHANGE);
   Serial.begin(9600);
+}
+
+void handleInterrupt() {
+  interruptTriggered = true;
+
+  int buttonState = digitalRead(buttonPin);
+  Serial.print("Button state: ");
+  Serial.println(buttonState);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED is ON");
+  } else {
+    digitalWrite(ledPin, LOW);
+    Serial.println("LED is OFF");
+  }
 }
 
 void loop() {
@@ -22,21 +36,8 @@ void loop() {
     Serial.println("Interrupt triggered!");
   }
 
-  int buttonState = digitalRead(buttonPin);
-  Serial.print("Button state: ");
-  Serial.println(buttonState);
-
   for (int i = 0; i < 10000; i++) {
     Serial.println("calculating...");
-    if (interruptTriggered) break;
-  }
-
-  if (buttonState == LOW) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("LED is ON");
-  } else {
-    digitalWrite(ledPin, LOW);
-    Serial.println("LED is OFF");
   }
 }
 
